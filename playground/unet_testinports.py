52d2fc442ff132f237310fd4d288c706a157d86b
Erling Amundsen <erling.amundsen@gmail.com>
2025-04-08 10:56:22 +0200

    made changes to imports to ensure compatability and started unet_test

---
+++ playground/unet_testinports.py
@@ -0,0 +1,12 @@
+import torch
+import torch.optim as optim
+from pathlib import Path
+from torch import nn
+import os
+from PIL import Image
+import numpy as np
+import pandas as pd
+from diffusers import AutoPipelineForText2Image
+import torch
+import testimports
+
