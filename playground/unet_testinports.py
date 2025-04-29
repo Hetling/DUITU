12cece35af207e2d9e83fe5ddf9033ea9223fbff
Christian Alexander Hetling <chrhe@itu.dk>
2025-04-29 10:27:18 +0200

    remove import

--- playground/unet_testinports.py
+++ playground/unet_testinports.py
@@ -6,7 +6,6 @@ import os
 from PIL import Image
 import numpy as np
 import pandas as pd
-from diffusers import AutoPipelineForText2Image
 import torch
 from testimports import CustomImageDataset
 from torchvision import transforms
